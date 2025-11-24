94e88fdc7626fb35c0b2a9d8ef450c7200c050b1
Charly ANTOINE <charly.antoine@etu-unicaen.fr>
2025-11-24 16:34:25 +0100

    Ajout de la page d'accueil / navbar / footer / css(mai et navbar)

---
+++ pages/objets/footer.php
@@ -0,0 +1,31 @@
+<!-- CSS Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- JS Bootstrap + Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<footer class="bg-dark text-white py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-3 d-flex flex-column justify-content-center align-items-center">
+                    <h5>DUCRET</h5>
+                    <p>Matthéo</p>
+                </div>
+                <div class="col-12 col-md-3 d-flex flex-column justify-content-center align-items-center">
+                    <h5>PARIS-LEMPERIERRE</h5>
+                    <p>Victor</p>
+                </div>
+                <div class="col-12 col-md-3 d-flex flex-column justify-content-center align-items-center">
+                    <h5>GILLOT-LETANG</h5>
+                    <p>Achille</p>
+                </div>
+                <div class="col-12 col-md-3 d-flex flex-column justify-content-center align-items-center">
+                    <h5>ANTOINE</h5>
+                    <p>Charly</p>
+                </div>
+            </div>
+        </div>
+        <div class="text-center mt-4">
+            <p>&copy; 2024 Dungeon Xplorer. Tous droits réservés.</p>
+        </div>
+    </footer>
---
+++ pages/objets/header.php
@@ -0,0 +1,25 @@
+<!-- CSS Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<link rel="stylesheet" href="navbar.css">
+
+<!-- JS Bootstrap + Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-container">
+            <a class="navbar-brand" href="index.php">
+                <h3 class="mb-0">DungeonXplorer</h3>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link active" href="index.php">Accueil</a></li>
+                    <li class="nav-item"><a class="nav-link" href="chapters.php">Chapitres</a></li>
+                    <li class="nav-item"><a class="nav-link" href="monsters.php">Monstres</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>

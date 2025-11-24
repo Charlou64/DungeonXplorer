369bffdc24b49b6b436f5b3bff8e84b75db2a3a3
Charly ANTOINE <charly.antoine@etu-unicaen.fr>
2025-11-24 16:15:42 +0100

    [FEATURES] page de creation de personnage

--- pages/connexion.php
+++ pages/connexion.php
@@ -4,10 +4,11 @@ $host = "https://dev-dx04.users.info.unicaen.fr/phpmyadmin/index.php?route=/&rou
 $nom_BDD = "dx04_bd";
 
 try {
-    $bdd = new PDO('mysql:host=host,dbname=dx04_db;charset=utf8', 'dx04', 'zohxoothega9eiS2');
+    $bdd = new PDO('mysql:host=localhost,dbname=dx04_db;charset=utf8', 'dx04', 'zohxoothega9eiS2');
     // Pour afficher les erreurs PDO en cas de problème :
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
+?>
---
+++ pages/creationperso.php
@@ -0,0 +1,16 @@
+<?php
+    require_once "connexion.php"
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creation de Personnage</title>
+</head>
+<body>
+    <h1>Créez votre personnage !</h1>
+</body>
+</html>
